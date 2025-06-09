1e084881eb55792913dc1b0306c21c7581f2c51b
Максим Сергеевич <88585076+Hramoff@users.noreply.github.com>
2025-06-09 16:27:29 +0300

    Update __init__.py

--- app/routes/__init__.py
+++ app/routes/__init__.py
@@ -1,5 +1,15 @@
+import importlib
+import inspect
+import pkgutil
 from flask import Blueprint
-from .main import main
 
-main_bp = Blueprint('main', __name__)
-main_bp.register_blueprint(main)
+def register_blueprints(app):
+    from . import __path__ as routes_path
+
+    for _, module_name, _ in pkgutil.iter_modules(routes_path):
+        module = importlib.import_module(f'app.routes.{module_name}')
+
+        for name, obj in inspect.getmembers(module):
+            if isinstance(obj, Blueprint):
+                app.register_blueprint(obj)
+                print(f"Registered blueprint: {name} from {module_name}")
